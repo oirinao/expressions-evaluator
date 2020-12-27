19381795a6115ee619e7cc90427effe0347c8236
oirinao <50031634+oirinao@users.noreply.github.com>
2020-12-27 20:29:33 +0200

    shunting yard

---
+++ calculator.js
@@ -0,0 +1,113 @@
+function fordelete(){
+    let output = document.getElementById("output").value;
+    document.getElementById("output").value = output.substr(0, output.length - 1);
+}
+
+function forclear(){
+    document.getElementById("output").value = "0";
+}
+
+function removeZero() {
+    var value = document.getElementById("output").value;
+    if (value == "0") {
+         value = " "
+         document.getElementById("output").value = value;
+    }
+}
+
+function fordisplay(value) {
+    removeZero();
+    document.getElementById("output").value += value;
+}
+
+
+var outputSelector  = document.getElementById("output");
+outputSelector.addEventListener("keyup", function (event) {
+    var char = String.fromCharCode(event.which).toLocaleLowerCase();
+    var acceptedTokens = "+-*/√()1234567890. "
+
+    var text = outputSelector.value;
+    for (let i = 0; i < text.length; i++) {
+        if (acceptedTokens.indexOf(text[i]) == -1) {
+            text = text.replace(text[i], '');
+        }
+    }
+    outputSelector.value = text;
+})
+
+addEventListener("keyup", function(event) {
+    console.log(event.keyCode);
+    switch (event.keyCode) {
+        case 13:
+            document.getElementById("r5-3").click();
+            break;
+        default:
+            break;
+    }
+});
+
+// addEventListener("keyup", function(event) {
+//     console.log(event.keyCode);
+//     switch (event.keyCode) {
+//         case 13:
+//             document.getElementById("myBtn").click();
+//             break;
+//         case 67: case 99:
+//             document.getElementById("r1-1").click();
+//             break;
+//         case 47:
+//             document.getElementById("r1-2").click();
+//             break;
+//         case 56: case 106:
+//             document.getElementById("r1-3").click();
+//             break;
+//         case 8:
+//             document.getElementById("r1-4").click();
+//             break;
+//         case 55:
+//             document.getElementById("r2-1").click();
+//             break;
+//         case 56:
+//             document.getElementById("r2-2").click();
+//             break;
+//         case 57:
+//             document.getElementById("r2-3").click();
+//             break;
+//         case 109: case 189:
+//             document.getElementById("r2-4").click();
+//             break;
+//         case 52:
+//             document.getElementById("r3-1").click();
+//             break;
+//         case 53:
+//             document.getElementById("r3-2").click();
+//             break;
+//         case 54:
+//             document.getElementById("r3-3").click();
+//             break;
+//         case 107: case 187:
+//             document.getElementById("r3-4").click();
+//             break;
+//         case 49:
+//             document.getElementById("r4-1").click();
+//             break;
+//         case 50:
+//             document.getElementById("r4-2").click();
+//             break;
+//         case 51:
+//             document.getElementById("r4-3").click();
+//             break;
+//         case 37:
+//             document.getElementById("r5-1").click();
+//             break;
+//         case 48:
+//             document.getElementById("r5-2").click();
+//             break;
+//         case 46:
+//             document.getElementById("r5-3").click();
+//             break;
+
+//         default:
+//             break;
+//     }
+// });
---
+++ sy2.js
@@ -0,0 +1,187 @@
+let i;
+let output = [];
+let stack = [];
+let resultStack = [];
+
+function peek(a){
+    return a[a.length - 1];
+}
+
+function solve(){
+    removeZero();
+    let infix = input("output");
+    //console.log(infix);
+
+    if (validate(infix)){
+        let postfix = toPostfix(infix);
+        console.log(postfix);
+
+        let output = calculate(postfix);
+        console.log(output);
+        document.getElementById("output").value = output;
+    }
+}
+
+function input(input){
+    let infix = document.getElementById(input).value.replace(/ /g, '')
+                .replace(/\++/g, '+')
+                .replace(/\-+/g, '-')
+                .replace(/\*+/g, '*')
+                .replace(/\/+/g, '/')
+                .replace(/\-\(+/g, '-1*(');
+    console.log("infix", infix);
+
+    return infix;
+}
+
+function validate(infix){
+    let openCount = 0;
+    let closeCount = 0;
+
+    for (let i = 0; i < infix.length; i++) {
+        if (infix[i] == '(')
+            openCount++;
+        else if(infix[i] == ')')
+            closeCount++;
+    }
+
+    if (openCount != closeCount){
+        alert("Error: \nUnequal number of opening and closing paranthesis");
+        return 0;
+    }
+    else
+        return 1;
+}
+
+function toPostfix(infix){
+    output = [];
+    stack = [];
+    resultStack = [];
+
+    for (i = 0; i < infix.length; i++) {
+        if (isNumber(infix)) {
+            output.push(getNumberAsString(infix));
+        }  
+        else if (isOperator(infix)) {
+            push(infix[i]);
+        }
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }    
+        else if (infix[i] == ')') {
+            while (peek(stack) != '('){
+                output.push(stack.pop());
+            }
+            stack.pop();
+        }
+    }
+    while (stack.length > 0){
+        output.push(stack.pop());
+    }
+
+    //console.log(output);
+    //console.log(stack);
+
+    return output;
+}
+
+function calculate(output){
+    while(output.length > 0) {
+        if (!isNaN(parseFloat(output[0]))){
+            let nr = parseFloat(output.shift());
+            resultStack.push(nr);
+        }
+        else{
+            operate(peek(resultStack), output[0], resultStack[resultStack.length - 2]);
+            output.shift();
+            resultStack.pop();
+        }
+        
+    }
+    return resultStack[0];
+}
+
+function operate(a, op, b){
+    if (op == '+')
+		resultStack[resultStack.length - 2] = b + a;
+	else if (op == '-')
+        resultStack[resultStack.length - 2] = b - a;
+	else if (op == '*')
+        resultStack[resultStack.length - 2] = b * a;
+	else if (op == '/')
+        resultStack[resultStack.length - 2] = b / a;
+}
+
+function push(temp){
+    if (stack.length == 0) {
+        stack.push(temp);
+    }
+    else if(temp == '('){
+        stack.push(temp);
+    }
+    else if(getGrade(temp) > getGrade(peek(stack))){
+        stack.push(temp);
+    }
+    else{
+        while(getGrade(peek(stack)) >= getGrade(temp) && stack.length > 0){
+            output.push(peek(stack));
+            stack.pop();
+        }
+        push(temp);
+    }
+        
+}
+
+function getGrade(temp){
+    if (temp == '-' || temp == '+') {
+        return 1;
+    }
+    else if(temp == '*' || temp == '/'){
+        return 2;
+    }
+    else
+        return 0;
+}
+
+function getNumberAsString(infix){
+    let temp = "";
+    let tokens = ['+', '-', '*', '/', '(', ')'];
+    
+    temp += infix[i];
+    i++;
+
+    while (tokens.indexOf(infix[i]) == -1 && i < infix.length ){
+        temp += infix[i];
+        i++;
+    }
+    i--;
+
+    return String(temp);
+}
+
+function isNumber(infix){
+    if (infix[i] >= 0 && infix[i] <= 9) {
+        return 1;
+    }
+    else if ((infix[i] == '-' || infix[i] == '+' && !isNaN(parseFloat(infix[i+1]))) && i == 0){
+        return 1;
+    }
+    else if ((infix[i] == '-' || infix[i] == '+' && !isNaN(parseFloat(infix[i+1]))) && infix[i-1] == '('){
+        return 1;
+    }
+    else
+        return 0;
+}
+
+function isOperator(infix){
+    let ops = ['+', '-', '*', '/'];
+    if (ops.indexOf(infix[i] != -1) ) {
+        if (!isNaN(parseFloat(infix[i+1]))) {
+            return 1;
+        }
+        else if (infix[i+1] == '(') {
+            return 1;
+        }
+    }
+    return 0;
+}
